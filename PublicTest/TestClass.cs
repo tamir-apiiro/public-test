159c710a04c038c91b4444b9648c3b72828404a6
tamirl <tamir@apiiro>
2021-10-11 17:56:38 +0300

    returned to last thing that recreated the bug

--- PublicTest/TestClass.cs
+++ PublicTest/TestClass.cs
@@ -10,9 +10,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DefaultNamespace
 {
-    public class Base
+    public abstract class Base
     {
         [HttpGet("{key}/riskTriggers")]
+        [AuthorizeResource(RoleResource.Risk)]
         public async Task<ProfileRiskTriggers> GetProfileRiskTriggersAsync(string key)
         {
             return null;
@@ -23,31 +24,44 @@ namespace DefaultNamespace
         {
         }
     }
-    public class TestClass : Base
+    public abstract class TestClass : Base
     {
         [HttpGet("{key}/riskTriggers")]
+        [AuthorizeResource(RoleResource.Risk)]
         public async Task<ProfileRiskTriggers> GetProfileRiskTriggersAsync(string key)
         {
             return null;
         }
-        //
-        // [HttpGet("{a}/b")]
-        // public void Foo(string a)
-        // {
-        // }
-        //
-        // [HttpPost("{c}/d")]
-        // public void Bar(string c)
-        // {
-        // }
-        //
-        // [HttpPost("e")]
-        // public void Func()
-        // {
-        // }
+        
+        [HttpGet("{a}/b")]
+        public void Foo(string a)
+        {
+        }
+        
+        [HttpPost("{c}/d")]
+        public void Bar(string c)
+        {
+        }
+
+        [HttpPost("e")]
+        public void Func()
+        {
+        }
     }
 
     public class ProfileRiskTriggers
     {
+    }
+    
+    public class AuthorizeResourceAttribute : AuthorizeAttribute
+    {
+        public AuthorizeResourceAttribute(RoleResource resourceType) : base(resourceType.ToString())
+        {
+        }
+    }
+
+    public enum RoleResource
+    {
+        Risk
     }
 }
